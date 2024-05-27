b1d6b03f35c4725a20e8b10d242966a25b42598e
JUMINAHN <jumin9774@naver.com>
2024-05-27 17:30:36 +0900

    may27th

---
+++ src/com/winter/study1/Study8_2.java
@@ -0,0 +1,28 @@
+package com.winter.study1;
+
+import java.util.Scanner;
+//ctrl + f11
+public class Study8_2 {
+	public static void main(String[]args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int tan = 3;
+		
+		for(int i=0; i<tan; i++) {
+			System.out.println("모드를 선택하세요 1.단발모드 2.점사모드");
+			int select = sc.nextInt();
+			if(select == 1) {
+				for(int j=0; j<30; j++) {
+					System.out.println("탕" + (j+1) + "번");
+				}//안쪽 for의 끝이다.
+			} else {
+				for(int j=0; j<10; j++) {
+					System.out.println("타타탕" + (j+1) + "번");
+				}//안쪽 for의 끝이다.
+			}
+		}
+		System.out.println("장전을 종료합니다.");
+		
+		
+	}
+}
--- src/com/winter/study1/Study8_3.java
+++ src/com/winter/study1/Study8_3.java
@@ -3,7 +3,7 @@ package com.winter.study1;
 import java.util.Scanner;
 
 //ctrl + f11
-public class Study8 {
+public class Study8_3 {
 	public static void main(String[] args) {
 		//원하는 결과물이 무엇인지 생각해봐야함
 		Scanner sc = new Scanner(System.in);
@@ -13,20 +13,22 @@ public class Study8 {
 			System.out.println("모드를 선택하세요 1.단발모드 2.점사모드");
 			int select = sc.nextInt();
 			
-			int result = 10;
-			String sth = "타타탕";			
+			int result = 0;
+			String sth = "";
+			
 			if (select == 1) {
 				result = 30;
 				sth = "탕";
-			} 
+			} else {
+				result = 10;
+				sth = "탕탕탕";
+			}
+
 			for (int j = 0; j < result; j++) {
 				System.out.println(sth + (j + 1) + "번");
 			}
 		}
 		System.out.println("장전을 종료합니다.");
-		//문법이나 생각하는 것을 늘리기 위해서 진행할 것
-		//백엔드 코드는 정해져있다. --> 문제를 꼬아서 만들고,,
-		//생각하는 사고를 기르기 + 문법
 
 	}
 }
