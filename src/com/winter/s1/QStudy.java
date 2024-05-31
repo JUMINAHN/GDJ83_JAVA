b64d6b88b0fb56618f1011ef58eb6368425b59be
JUMINAHN <jumin9774@naver.com>
2024-05-31 17:40:49 +0900

    숙제

---
+++ src/com/winter/s1/QStudy.java
@@ -0,0 +1,20 @@
+package com.winter.s1;
+
+public class QStudy { // 선입선출
+	public int[] add(int[] ar) {
+		// 스택구조 -> 먼저들어온게 맨끝, 오른쪽이 끝
+		// 처음에 0, 1이라는 데이터가 쌓이는 것 : 왼쪽에 1 0
+		// 2 1 0
+
+		// 받은 배열에서 하나의 임의의 데이터를 추가
+		// 제일 앞에 추가되는 것 -> unshift처럼
+		// 추가된 배열을 return하자
+
+		return;// 인트배열타입
+	}
+
+	public int[] remove(int[] ar) {
+		// 하나의 요소를 삭제 -> 맨 뒤의 요소 삭제
+
+	}
+}
