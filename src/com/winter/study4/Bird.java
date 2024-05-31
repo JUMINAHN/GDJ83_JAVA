a39cf39a30f5b69c2cb3d510e4ad3b5a42de57ec
JUMINAHN <jumin9774@naver.com>
2024-05-31 10:26:39 +0900

    May 31th : static / final / abstract 설명

---
+++ src/com/winter/study4/Bird.java
@@ -0,0 +1,13 @@
+package com.winter.study4;
+
+public abstract class Bird { // 이 클래스는 미완성의 클래스다. -> 변경해야한다는 의미
+	// 선언은 되나, 객체를 만들 수 없다. --> 추상 클래스
+	int age;
+
+	// abstract -> 추상.. = 변경해야됨
+	// final과 반대의 의미
+
+	public abstract void fly(); // 미완성의 메서드 -> 추상 메서드
+	// 상속받아서 사용하세요~
+
+}
--- src/com/winter/study4/Car.java
+++ src/com/winter/study4/Car.java
@@ -2,10 +2,12 @@ package com.winter.study4;
 //그외 제어자 이해하기
 
 //**static : 객첼 생성하지 않고 사용 가능, 하나의 프로그램 안에 공통으로 사용할 때  --> 멤버변수 / 멤버메서드
-public class Car {
+public final class Car { // final 선언시 클래스를 변경하지마라라고 함 --> 상속하지마라****** 있는 그대로를 써라
+
 	// 접근지정자 그외지정자 데이터타입 변수명
 	// 클래스 변수
 	// 대부분 자체 초기화를 진행한다.
+	// static + final 조합 많이 씀
 	public static String company; // *변수명이 기울어졌다는건 static이 붙은 것 -> 객체를 만들지 않고 쓴다.
 	// 객체를 안만들면 주소가 없음 , 근데 주소를 알아야 찾아갈 수 있다. --> 메서드영역(클래스)
 	// stack이 먼저 차지 -> heap(만들어질수도 안될수도)
@@ -20,7 +22,12 @@ public class Car {
 		// static 초기화 블록이 따로있음 -> 이게 있냐 없나의 차이지만, 이걸 잘 보지는 않을 것임
 	}
 
-	public final int price = 30; // final : 한번데이터 값은 변경이 불가하다. -> 초기화 안할 시 에러가 뜬다.
+	public final int MAX_PRICE; // final : 한번데이터 값은 변경이 불가하다. -> 초기화 안할 시 에러가 뜬다.
+	// 상수들은 보통 대문자로 사용한다.
+
+	public Car() {
+		this.MAX_PRICE = 100; // final 은 값을 초기화하면 상수처럼 변경을 못하게 해라
+	}
 
 	// static은 instance 접근 불가
 	public static void info() { // 객체를 만들지 않고 쓸 수 있다. -> Main 시작전부터
@@ -32,7 +39,8 @@ public class Car {
 	}
 
 	// instance는 static instance 모두 접근 가능
-	public void info2() {
+	public final void info2() { // 메서드를 변경하지마라 -> 상속과 관련
+
 		Car.info(); // 메서드 내에서 다른 메서드 호출 가능(main이랑 똑같음)
 		// 프로그램 시작 전부터 만들어져있으니까 사용가능
 		System.out.println(Car.company);
