7ad2da02a8f27f673a0670e8ac680d43d6c896f2
JUMINAHN <jumin9774@naver.com>
2024-06-03 10:03:06 +0900

    1교시

--- src/com/winter/s1/lang/S1Main.java
+++ src/com/winter/s1/lang/S1Main.java
@@ -1,7 +1,5 @@
 package com.winter.s1.lang; //이클래스 현 패키지
 
-import java.util.Scanner; //Scanner가 어디있는 클래스인지 알려줄 때 
-
 public class S1Main {
 
 	public static void main(String[] args) {
@@ -9,9 +7,21 @@ public class S1Main {
 		// 1)같은 패키지 내에 있는 클래스는 생략
 		// 2)java.lang 내에 있는 클래스를 사용할 때 import 생략
 		Object obj = new Object(); // Object도 클래스이자 오브젝트
-		Scanner sc = new Scanner(System.in);
-		// Stringbuilder -> final? -> 메서드체이닝*
+		Object obj2 = new Object();
+		System.out.println(obj == obj2); // 주소값끼리 비교
 
-		StringBuffer st = new StringBuffer();
+		// String 알고가기
+		String n = new String("winter");
+		String n2 = "winter"; // 이것또한 참조변수
+
+		System.out.println(n == n2); // 주소값끼리 비교
+		// 참조변수를 찍으면 toString()을 자동호출한다 -> 주소값을 호출 -> 모든클래스는 오브젝트
+		// toString, eqauls 오버라이딩
+		// method를 호출할 수 있어야 함
+
+		char ch = n.charAt(0); // 문자의 0번째 목록 출력
+		System.out.println(ch);
+		int num = n.length();
+		System.out.println(num);
 	}
 }
