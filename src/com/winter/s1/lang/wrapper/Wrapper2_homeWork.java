4a686e338e197d0120e2807df7da791d83712335
JUMINAHN <jumin9774@naver.com>
2024-06-04 18:06:55 +0900

    Homework_대소문자검증완료

--- src/com/winter/s1/lang/wrapper/Wrapper2_homeWork.java
+++ src/com/winter/s1/lang/wrapper/Wrapper2_homeWork.java
@@ -13,21 +13,24 @@ public class Wrapper2_homeWork {
 		// 둘다 있어야 함 -> 하나하나 검증,, contain안쓰고
 		// 대문자 아닌건 걍 -> 빠지고, 대문자인것들만 charAt()으로 검증하면 되지 않을까?
 		// -> 그리고 그뒤에 길이 검증하면 되지 않을까?
-
-		boolean flag = true;
+		String flag = "최소 1개 이상의 대/소문자가 있어야 합니다.";
+		char[] result = new char[pw.length()];
 		for (int i = 0; i < pw.length(); i++) {
-			char c = pw.charAt(i);
-			if (!Character.isUpperCase(c)) {
-				flag = false;
-			} else {
+			char ch = pw.charAt(i);
+			if (Character.isUpperCase(ch)) { // 대문자면 true -> 아니면 false --> Character
+				result[i] = ch;
+			}
+		}
+
+		char[] result2 = new char[result.length];
+		for (int i = 0; i < result.length; i++) {
+			if (Character.isUpperCase(result[i])) { // 대문자면 true -> 아니면 false
+				result2[i] = result[i];
+				flag = "통과";
 				break;
 			}
 		}
-		if (flag) {
-			System.out.println("대문자 한 개 이상 발견!");
-		} else {
-			System.out.println("대문자는 한 개 이상 있어야 합니다.");
-		}
-
+		System.out.println(flag);
+		// 대소문자 검증 완료
 	}
 }
