97fd3925bd08a8cdb30c1cf9a72d5ef53518e9d8
JUMINAHN <jumin9774@naver.com>
2024-06-07 15:06:29 +0900

    5교시) 쉬는시간_Quiz완료

--- src/com/winter/s2/util/collections/ex/ProductMain.java
+++ src/com/winter/s2/util/collections/ex/ProductMain.java
@@ -7,12 +7,15 @@ public class ProductMain {
 	public static void main(String[] args) {
 		ProductService productService = new ProductService();
 		ArrayList<ProductDTO> ar = productService.init();
+		productService.addProudct(ar);
+		;
 
 		// 배열의 내용 가져오는 것 --> get 활용
 		for (int i = 0; i < ar.size(); i++) {
 			System.out.println(ar.get(i).getPrice());
 			System.out.println(ar.get(i).getName());
 			System.out.println(ar.get(i).getStock());
+			System.out.println("=================");
 		}
 
 	}
--- src/com/winter/s2/util/collections/ex/ProductService.java
+++ src/com/winter/s2/util/collections/ex/ProductService.java
@@ -1,6 +1,7 @@
 package com.winter.s2.util.collections.ex;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class ProductService {
@@ -30,13 +31,26 @@ public class ProductService {
 	}
 
 	// 기존의 가방에다가 새상품을 추가 --> ArrayList에
-	// void 이건 수정 불가
+	// void 이건 수정 불가 --> 선언부 수정 불가
 	// 하기에 새로운 상품을 만드는 것 --> 새로운 상품 new ProductDTO를 해라~
 	// 상품 이름도 없고 재고도 없는 것을 입력받아서 만든 상품을 기존의 가방에 추가하자~
 	// 기존 가방을 받아와서 해라~
 	// 추가하고 메인으로 돌아갔을 때 for문 돌아가서 찍었을 떄 새로운 제품까지 찍는다.
 	public void addProudct(ArrayList<ProductDTO> ar) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("상품의 가격을 입력하세요 : ");
+		Integer price = sc.nextInt();
+		System.out.println("상품명을 입력하세요 : ");
+		String name = sc.next();
+		System.out.println("상품의 수량을 입력하세요 : ");
+		Integer quantity = sc.nextInt();
 
+		ProductDTO pd = new ProductDTO();
+		pd.setName(name);
+		pd.setPrice(price);
+		pd.setStock(quantity);
+		// 넣은 것 배열에 추가
+		ar.add(pd);
 	}
 
 }
