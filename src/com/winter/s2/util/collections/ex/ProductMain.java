9dd3dc1815a9320909d13ac128973a44a384e215
JUMINAHN <jumin9774@naver.com>
2024-06-07 15:27:04 +0900

    6교시) 풀이 및 삭제 QUIZ 및 정답

--- src/com/winter/s2/util/collections/ex/ProductMain.java
+++ src/com/winter/s2/util/collections/ex/ProductMain.java
@@ -7,9 +7,12 @@ public class ProductMain {
 	public static void main(String[] args) {
 		ProductService productService = new ProductService();
 		ArrayList<ProductDTO> ar = productService.init();
-		productService.addProudct(ar);
-		;
+		// productService.addProudct(ar);
+		productService.removeProduct(ar);
 
+		// 이전 배열은 기존 배열을 받아서 한개늘린 배열을 새로만들고, 새로만든 배열을 return 해줬음
+		// 이젠 안해도 됨, 얘는 마음대로 늘렸다가 줄였다가 할 수 있음
+		// 기존 주소에 추가해서 늘리고 줄이고 하는 것이다.
 		// 배열의 내용 가져오는 것 --> get 활용
 		for (int i = 0; i < ar.size(); i++) {
 			System.out.println(ar.get(i).getPrice());
--- src/com/winter/s2/util/collections/ex/ProductService.java
+++ src/com/winter/s2/util/collections/ex/ProductService.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 
 public class ProductService {
 	private StringBuffer sb; // null값****
+	Scanner sc = new Scanner(System.in);
 
 	public ProductService() {
 		sb = new StringBuffer();
@@ -30,27 +31,28 @@ public class ProductService {
 		return ar;
 	}
 
-	// 기존의 가방에다가 새상품을 추가 --> ArrayList에
-	// void 이건 수정 불가 --> 선언부 수정 불가
-	// 하기에 새로운 상품을 만드는 것 --> 새로운 상품 new ProductDTO를 해라~
-	// 상품 이름도 없고 재고도 없는 것을 입력받아서 만든 상품을 기존의 가방에 추가하자~
-	// 기존 가방을 받아와서 해라~
-	// 추가하고 메인으로 돌아갔을 때 for문 돌아가서 찍었을 떄 새로운 제품까지 찍는다.
 	public void addProudct(ArrayList<ProductDTO> ar) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("상품의 가격을 입력하세요 : ");
-		Integer price = sc.nextInt();
-		System.out.println("상품명을 입력하세요 : ");
-		String name = sc.next();
+		ProductDTO productDTO = new ProductDTO();
+		System.out.println("상품명을 입력 : ");
+		productDTO.setName(sc.next());
+		System.out.println("가격 입력 : ");
+		productDTO.setPrice(sc.nextInt());
 		System.out.println("상품의 수량을 입력하세요 : ");
-		Integer quantity = sc.nextInt();
+		productDTO.setStock(sc.nextInt());
+		ar.add(productDTO);
+	}
 
-		ProductDTO pd = new ProductDTO();
-		pd.setName(name);
-		pd.setPrice(price);
-		pd.setStock(quantity);
-		// 넣은 것 배열에 추가
-		ar.add(pd);
+	// 기존의 가방을 받아서 삭제해라
+	// 일치하는 물건의 이름을 입력받아서 삭제 ---> return할 필요가 없음
+	public void removeProduct(ArrayList<ProductDTO> ar) {
+		System.out.println("찾을 상품을 입력해주세요 : ");
+		String item = sc.next();
+
+		for (int i = 0; i < ar.size(); i++) {
+			if (ar.get(i).getName().equals(item)) {
+				ar.remove(ar.get(i));
+			}
+		}
 	}
 
 }
