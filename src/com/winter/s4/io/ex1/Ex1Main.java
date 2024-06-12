2c507a1c74c52ccc4f13571330dd90e22dff821d
JUMINAHN <jumin9774@naver.com>
2024-06-12 14:22:54 +0900

    5교시) 만든 DTO를 리턴해주세요_QUIZ ++ 내가 푼 것

--- src/com/winter/s4/io/ex1/Ex1Main.java
+++ src/com/winter/s4/io/ex1/Ex1Main.java
@@ -1,8 +1,17 @@
 package com.winter.s4.io.ex1;
 
+import java.util.ArrayList;
+
 public class Ex1Main {
 	public static void main(String[] args) {
 		ReadStudy r = new ReadStudy();
-		r.read();
+		ArrayList<MenuDTO> ar = r.read();
+		for (MenuDTO a : ar) {
+			System.out.println(a.getMenuName());
+			System.out.println(a.getKcal());
+			System.out.println(a.getPrice());
+			System.out.println("==========");
+		}
+		System.out.println("종료");
 	}
 }
--- src/com/winter/s4/io/ex1/ReadStudy.java
+++ src/com/winter/s4/io/ex1/ReadStudy.java
@@ -4,27 +4,36 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class ReadStudy {
 
-	public void read() { // 이 메서드를 호출하면 파일을 읽어올 것임
+	public ArrayList<MenuDTO> read() { // 이 메서드를 호출하면 파일을 읽어올 것임
 		// info Text파일을 읽어오기
 		// 한줄씩 일어오면 안되니까
 		// 1.항목별로 파씽 -> 읽어서 파씽 후 출력
+		// 2.DTO를 만든후에 (메뉴수대로 --> 리턴을 해줌) DTO를 리턴해줌 --> main에서 출력후 확인만
 		File file = new File("C:\\study", "info.txt");
 		FileReader fr = null;
 		BufferedReader br = null;
+		ArrayList<MenuDTO> ar2 = new ArrayList();
 		try {
 			fr = new FileReader(file); // 예외 발생
 			br = new BufferedReader(fr);
 			while (true) {
+				MenuDTO m = new MenuDTO();
 				String s = br.readLine(); // 예외 발생 --> 한줄씩 읽으니까
-				String[] ar = s.split(",");
 				if (s == null) {
 					break;
 				}
-				for (String ss : ar) {
-					System.out.println(ss.trim());
+				StringTokenizer st = new StringTokenizer(s, ",");
+//				String[] ar = s.split(",");
+				while (st.hasMoreTokens()) {
+					m.setMenuName(st.nextToken().trim());
+					m.setKcal(Integer.parseInt(st.nextToken().trim()));
+					m.setPrice(Integer.parseInt(st.nextToken().trim()));
+					ar2.add(m);
 				}
 			}
 			// 반복문을 돌리자 --> 문자열로 출력
@@ -38,6 +47,7 @@ public class ReadStudy {
 				e.printStackTrace();
 			}
 		}
-		System.out.println("종료");
+
+		return ar2;
 	}
 }
