2e41376df4ba334599aa20765c191c471c62ced2
JUMINAHN <jumin9774@naver.com>
2024-06-13 15:38:47 +0900

    풀이

--- src/com/winter/s4/io/FileWriteMain.java
+++ src/com/winter/s4/io/FileWriteMain.java
@@ -13,34 +13,33 @@ public class FileWriteMain {
 		// 문자열 -> 문자 -> 01
 		Scanner sc = new Scanner(System.in);
 		// boolean flag = true;
-		while (true) {
-			System.out.println("저장할 파일명을 입력, 원하지 않을 시 E or e : ");
-			String fileName = sc.next();
-			if (fileName.equals("E") || fileName.equals("e")) {
-				break;
-			}
-			File file = new File("C:\\study", fileName); // 덮어쓰기가 됨
-			FileWriter fw = null;
-			try {
-				// 기존에 파일명이 있다면 기본은 덮어 씌우기 (false)
-				// append 즉 추가 (기존 내용 연결) -> false
+		System.out.println("저장할 파일명을 입력 : ");
+		String fileName = sc.next();
+		File file = new File("C:\\study", fileName); // 덮어쓰기가 됨
+		FileWriter fw = null;
+		try {
+			// 기존에 파일명이 있다면 기본은 덮어 씌우기 (false)
+			// append 즉 추가 (기존 내용 연결) -> false
+			while (true) {
 				fw = new FileWriter(file, true);
 				System.out.println("내용을 입력하세요 : ");
 				String s = sc.next();
+				if (s.equals("E") || s.equals("e")) { // upper lower 다 상관없음
+					break;
+				}
 				fw.write(s + "\r\n"); // write는 보조 스트림을 쓸 필요가 없게 String을 담을 수 있는 타입이 있음 --> 작성
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				fw.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} finally {
-				try {
-					fw.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
 			}
 		}
 		System.out.println("프로그램 종료");
-
 	}
 }
